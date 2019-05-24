9843bb485427ebc3ef6618fd6b4eab4eeae0e59c
amritsrivastava <srivastava.amrit1999@gmail.com>
2019-05-24 15:50:17 +0530

    bootstrap and material ui setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Paper from '@material-ui/core/Paper';
+// import { styled } from '@material-ui/styles'
+import mainstyled from 'styled-components'
+import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { withStyles } from '@material-ui/styles';
+
+const useStyles = {
+  root: {
+    padding: '20px',
+    margin: '20px',
+    backgroundColor: 'blue'
+  },
+};
+
+const MyBox = mainstyled.div`
+  background-color: black;
+  color: white; 
+`
+
+// const MyPaper = styled(Paper)({
+//   backgroundColor: 'pink'
+// }) 
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">   
+      <MyBox>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </MyBox>
+  </div>
+    )
+  }
 }
 
-export default App;
+export default withStyles(useStyles)(App);
