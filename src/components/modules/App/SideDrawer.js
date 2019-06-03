42a1d316991789cb64dd7d4d15fa588cb94539ba
amritsrivastava <srivastava.amrit1999@gmail.com>
2019-06-03 10:00:26 +0530

    Create Side Drawer

--- src/components/modules/App/SideDrawer.js
+++ src/components/modules/App/SideDrawer.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Material UI
 import { withStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Typography from '@material-ui/core/Typography';
+
+// File Imports
+import InputDialog from '../../widgets/InputDialog';
+import Avtar from '../../../assets/img/avtar/amrit.jpeg';
+import QR from '../../../assets/img/qr.png';
+import Button from '../../widgets/Button';
 
 const styles = {
   list: {
@@ -19,36 +23,47 @@ const styles = {
 };
 
 class SideDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClickOpen() {
+    this.state.open
+      ? this.setState({ open: false })
+      : this.setState({ open: true });
+  }
+  handleClose() {
+    this.setState({ open: false });
+  }
+
   render() {
     const { classes } = this.props;
     const sideList = (
       <div
-        className={classes.list}
+        className="side-drawer text-center justify-content-center"
         role="presentation"
         onClick={this.props.handleClose}
         onKeyDown={this.props.handleClose}
       >
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        <img src={Avtar} alt="avtar" className="avtar my-4" />
+        <Typography variant="h5">Full Name</Typography>
+        <Typography variant="h6" className="mb-2">
+          sample@email.com
+        </Typography>
         <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        <img src={QR} alt="QR code" className="col-8" />
+        <Divider />
+        <Button className="mt-5 mb-2 col-8" onClick={this.handleClickOpen}>
+          Share Your Card
+        </Button>
+        <Link to="/create-card">
+          <Button className=" mt-2 mb-3 col-8">Edit Your Card</Button>
+        </Link>
       </div>
     );
 
@@ -61,6 +76,17 @@ class SideDrawer extends React.Component {
         >
           {sideList}
         </SwipeableDrawer>
+        <InputDialog
+          open={this.state.open}
+          handleClose={this.handleClose}
+          title="Share Card"
+          input={{
+            id: 'email-username',
+            label: 'Email/Username',
+            type: 'text'
+          }}
+          buttonLabel="Submit"
+        />
       </div>
     );
   }
